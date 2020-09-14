fa8ba1fd608aac93967e494573367363bf7cb506
drakmaniso <moussault.laurent@gmail.com>
2020-09-14 15:07:42 +0200

    Update the scanner

--- src/scanner.c
+++ src/scanner.c
@@ -9,6 +9,7 @@ typedef struct state {
 
 state lex_between (Scanner *s);
 state lex_identifier (Scanner *s);
+state lex_delimiter (Scanner *s);
 state lex_operator (Scanner *s);
 state lex_number (Scanner *s);
 state lex_invalid (Scanner *s);
@@ -16,8 +17,8 @@ state lex_invalid (Scanner *s);
 bool is_white_space (Rune r);
 bool is_letter (Rune r);
 bool is_digit (Rune r);
+bool is_delimiter (Rune r);
 bool is_symbol (Rune r);
-bool is_keyword (String s);
 
 
 // SCANNER
@@ -85,28 +86,40 @@ void Scanner_print (Token t) {
 	}
 
 	switch (t.tag) {
-		case Token_Keyword:
-			printf ("kw<");
-			String_print (t.value);
-			printf (">");
+		case Token_Public:
+			printf ("public");
+			break;
+
+		case Token_Do:
+			printf ("do");
+			break;
+
+		case Token_End:
+			printf ("end");
+			break;
+
+		case Token_If:
+			printf ("if");
+			break;
+
+		case Token_Then:
+			printf ("then");
+			break;
+
+		case Token_Else:
+			printf ("else");
 			break;
 
 		case Token_Identifier:
-			printf ("id<");
+			printf ("\"");
 			String_print (t.value);
-			printf (">");
+			printf ("\"");
 			break;
 
 		case Token_Number:
-			printf ("num<");
+			printf ("\"");
 			String_print (t.value);
-			printf (">");
-			break;
-
-		case Token_Operator:
-			printf ("op<");
-			String_print (t.value);
-			printf (">");
+			printf ("\"");
 			break;
 
 		case Token_Equal:
@@ -117,9 +130,27 @@ void Scanner_print (Token t) {
 			printf ("->");
 			break;
 
+		// Delimiters
+
 		case Token_Semicolon:
 			printf (";\n");
 			return;
+			break;
+
+		case Token_LeftParen:
+			printf ("(");
+			break;
+
+		case Token_RightParen:
+			printf (")");
+			break;
+
+		case Token_Comma:
+			printf (",");
+			break;
+
+		case Token_Quote:
+			printf ("\'");
 			break;
 
 		case Token_Invalid:
@@ -161,9 +192,8 @@ state lex_between (Scanner *s) {
 			continue;
 		}
 
-		if (r == ';') {
-			emit (s, Token_Semicolon);
-			return (state) {lex_between};
+		if (is_delimiter (r)) {
+			return (state) {lex_delimiter};
 		}
 
 		if (is_letter (r)) {
@@ -201,8 +231,20 @@ state lex_identifier (Scanner *s) {
 		}
 
 		backup (s);
-		if (is_keyword (String_slice (s->input, s->start, s->pos))) {
-			emit (s, Token_Keyword);
+
+		String text = String_slice (s->input, s->start, s->pos);
+		if (String_is (text, "public")) {
+			emit (s, Token_Public);
+		}else if (String_is (text, "do")) {
+			emit (s, Token_Do);
+		} else if (String_is (text, "end")) {
+			emit (s, Token_End);
+		} else if (String_is (text, "if")) {
+			emit (s, Token_If);
+		} else if (String_is (text, "then")) {
+			emit (s, Token_Then);
+		} else if (String_is (text, "else")) {
+			emit (s, Token_Else);
 		} else {
 			emit (s, Token_Identifier);
 		}
@@ -224,7 +266,31 @@ state lex_operator (Scanner *s) {
 		return (state) {lex_between};
 	}
 
-	emit (s, Token_Operator);
+	emit (s, Token_Invalid);
+	return (state) {lex_between};
+}
+
+
+state lex_delimiter (Scanner *s) {
+	switch(String_at(s->input, s->start)) {
+		case '\'':
+			emit (s, Token_Quote);
+			break;
+		case '(':
+			emit (s, Token_LeftParen);
+			break;
+		case ')':
+			emit (s, Token_RightParen);
+			break;
+		case ',':
+			emit (s, Token_Comma);
+			break;
+		case ';':
+			emit (s, Token_Semicolon);
+			break;
+		default:
+			emit (s, Token_Invalid);
+	}
 	return (state) {lex_between};
 }
 
@@ -276,6 +342,19 @@ bool is_digit (Rune r) {
 }
 
 
+bool is_delimiter (Rune r) {
+	return r == '\''
+		|| r == ';'
+		|| r == ','
+		|| r == '('
+		|| r == ')'
+		|| r == '['
+		|| r == ']'
+		|| r == '{'
+		|| r == '}';
+}
+
+
 bool is_symbol (Rune r) {
 	return r == '!'
 		|| r == '#'
@@ -285,7 +364,9 @@ bool is_symbol (Rune r) {
 		|| r == '*'
 		|| r == '+'
 		|| r == '-'
+		|| r == '.'
 		|| r == '/'
+		|| r == ':'
 		|| r == '<'
 		|| r == '='
 		|| r == '>'
@@ -295,9 +376,4 @@ bool is_symbol (Rune r) {
 		|| r == '^'
 		|| r == '|'
 		|| r == '~';
-}
-
-
-bool is_keyword (String s) {
-	return String_is (s, "func");
 }
