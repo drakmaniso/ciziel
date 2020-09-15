133172bdcf5dbdcaaa6902a46c961ce7da0cf994
drakmaniso <moussault.laurent@gmail.com>
2020-09-15 14:10:41 +0200

    Switch to rust
    It will probably be way easier to implement the type inference algorithm
    in rust.

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs;
+
+mod syntax;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("usage: {} <filename>", &args[0]);
+    } else {
+        let filename = &args[1];
+        let contents = fs::read_to_string(filename);
+        match contents {
+            Err(e) => {
+                println!("unable to read file {}: {}", filename, e);
+            }
+            Ok(source) => {
+                syntax::scan(filename.to_string(), source);
+            }
+        }
+    }
+}
